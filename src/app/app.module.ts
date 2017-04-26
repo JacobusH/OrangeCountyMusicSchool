ea84f16d14ac7f574fe54c242a47365b63edf63c
Jacobus Harding <jacobus.harding@metafuse.com>
2017-04-26 13:41:06 -0700

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+import { AlertModule } from 'ngx-bootstrap';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { MailingListComponent } from './mailing-list/mailing-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    AnnouncementsComponent,
+    MailingListComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
